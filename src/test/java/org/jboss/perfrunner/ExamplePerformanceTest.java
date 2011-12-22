b4cf2fdf2a4706c53239e2dcfb9d70c80ea52974
Jonathan Fuerth <jfuerth@gmail.com>
2011-12-22 18:34:53 -0500

    Further examples

--- src/test/java/org/jboss/perfrunner/ExamplePerformanceTest.java
+++ src/test/java/org/jboss/perfrunner/ExamplePerformanceTest.java
@@ -1,5 +1,7 @@
 package org.jboss.perfrunner;
 
+import java.util.Random;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -12,5 +14,28 @@ public class ExamplePerformanceTest {
       @Varying(axis=Axis.SERIES, name="This will appear in the legend") int x
       ) throws InterruptedException {
     Thread.sleep(sleepTime);
+  }
+
+  @Test
+  public void testMultiSleep(
+      @Varying(axis=Axis.X, name="Sleep Time", from=0, to=50, step=5) int sleepTime,
+      @Varying(axis=Axis.SERIES, name="Calls to sleep()", from=1, to=10) int calls
+      ) throws InterruptedException {
+    for (int i = 0; i < calls; i++) {
+      Thread.sleep((sleepTime/calls));
+    }
+  }
+
+  @Test
+  public void testMultiSleepWithRandom(
+      @Varying(axis=Axis.X, name="Sleep Time", from=0, to=50, step=5) int sleepTime,
+      @Varying(axis=Axis.SERIES, name="Calls to sleep()", from=1, to=10) int calls,
+      @Varying(axis=Axis.SERIES, name="Randomness", from=0, to=10, step=2) int randomness
+      ) throws InterruptedException {
+    Random r = new Random();
+    for (int i = 0; i < calls; i++) {
+      int randomAddition = randomness == 0 ? 0 : r.nextInt(randomness);
+      Thread.sleep((sleepTime/calls + randomAddition));
+    }
   }
 }
